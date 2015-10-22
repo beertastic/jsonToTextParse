ede8d8f938c56f4d14facf8e2d58329d9be9461e
beertastic <beertastic@gmail.com>
2015-10-22 08:58:05 +0100

    Added helper functions

--- json.php
+++ json.php
@@ -1,13 +1,19 @@
 <?php    
 
-	public function loopArray($myArray, $level = 0) {
+
+    public function convertTest() {
+        $myArray = json_decode($this->sample_data);
+        echo $this->loopArray($myArray);
+    }
+
+    public function loopArray($myArray, $level = 0) {
         $output = '';
  
         foreach($myArray as $key => $value) {
 
             if (!is_int($key)) {
                 $output .= '<br />' . $this->numberToDash($level). ' ' . $key;
-            }            
+            }
 
             if ((!is_object($value)) && (!is_array($value))) {
                 $output .= ' : ' . $value;
@@ -28,4 +34,12 @@
         }
 
         return $output;
+    }
+
+    public function numberToDash($count) {
+        $dash = '';
+        for ($x = 0; $x < $count; $x++) {
+            $dash .= ' &#8594; ';
+        }
+        return $dash;
     }
